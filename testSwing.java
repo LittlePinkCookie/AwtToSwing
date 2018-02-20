aaadc065e86698362cc11baccb69143593c5c03d
LittlePinkCookie <alouache.loan@gmail.com>
2018-02-20 14:52:58 +0100

    New build

---
+++ testSwing.java
@@ -0,0 +1,16 @@
+import java.*;
+
+public class testSwing extends JFrame
+{
+	private JPanel lol;
+	private JPanel mdr;
+	private JButton bite;
+	private JLabel encoreBite;
+
+	public testSwing() { }
+
+	public static void main()
+	{
+		new testSwing();
+	}
+}
